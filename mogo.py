d08f4aa5f3e6e1381b9b69849cf1a31aecd8ae95
jonasMapuche <jonasmapuche@gmail.com>
2020-09-04 21:11:22 -0300

    commit

--- mogo.py
+++ mogo.py
@@ -1,7 +1,16 @@
 from pymongo import MongoClient
+import json, datetime
 client = MongoClient('mongodb+srv://usuario:freedown@cluster0.28oko.azure.mongodb.net/stomach?retryWrites=true&w=majority')
 print(client.list_database_names())
 bd = client['stomach']
 collection = bd['letter']
-query = collection.find_one()
-print(query)
+query = collection.find({'frase.name': {'$eq': 'lesson 1'}})
+for doc in query:
+    #lista = client['stomach'].get_collection('letter').find({}).limit(1)
+    print('\n id: ', doc['_id'])
+    for sub in doc['frase']:
+        print(' frase: ', sub['name'])
+        for ora in sub['oracao']:
+            print('\nsujeito: ', ora['sujeito'])
+            print('\npredicado: ', ora['predicado'])
+    #print(lista)
