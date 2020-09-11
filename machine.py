91548cdbe596e59b2e307ad883aa2b5ad909c0fe
jonasMapuche <jonasmapuche@gmail.com>
2020-09-10 23:35:18 -0300

    commit

--- machine.py
+++ machine.py
@@ -11,7 +11,16 @@ import pandas
 print('pandas: {}'.format(pandas.__version__))
 import sklearn
 print('sklearn: {}'.format(sklearn.__version__))
-
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
 url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv'
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
@@ -39,6 +48,27 @@ print('* y')
 print(y)
 validationSize = 0.20
 seed = 7
-from sklearn import model_selection
-xTrain, xValidation, yTrain, yValidation = model_selection(x, y, test_size=validationSize, ramdon_state=seed)
+xTrain, xValidation, yTrain, yValidation = model_selection.train_test_split(x, y, test_size=validationSize, random_state=seed)
 scoring = 'accuracy'
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+results = []
+names = []
+for name, model in models:
+   kfold = model_selection.KFold(n_splits=10, random_state=seed)
+   cv_results = model_selection.cross_val_score(model, xTrain, yTrain, cv=kfold, scoring=scoring)
+   results.append(cv_results)
+   names.append(name)
+   msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+   print(msg)
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
